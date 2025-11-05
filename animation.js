142bf680eeff0918862fd407ae61114f29fcaacc
OCTerry <taaniyu@gmail.com>
2025-11-05 21:25:43 +0100

    GROS CHANGEMENTS

--- animation.js
+++ animation.js
@@ -1,16 +1,33 @@
 changeWidth
 
 function changeWidth() {
-    var scroll = window.pageYOffset / 5 +1 ;
+    var ratio = window.innerWidth / window.innerHeight //Ratio de la fenetre
+    var scroll = window.pageYOffset / ratio ;
     var width = 80 - scroll;
     var opacity = 100 - (scroll*10)
     if (opacity < 0){ 
         opacity = 0
     }
 
-    if (width >= 40) {
-        document.getElementById('resizable_title_div').style.width = width + 'vw';
-        document.getElementById('title_h2').style.opacity = opacity + '%'
+    if (ratio >= 1) {
+        if (width <= 100) {
+            document.getElementById('resizable_title_div').style.width = width + 'vw';
+            document.getElementById('title_h2').style.opacity = opacity + '%'
+        }
+        else {
+            document.getElementById('resizable_title_div').style.width = '40vw';
+            document.getElementById('title_h2').style.opacity = '0%'
+        }
+    }
+    else {
+        if (width <= 100) {
+            document.getElementById('resizable_title_div').style.width = width + 'vw';
+            document.getElementById('title_h2').style.opacity = opacity + '%'
+        }
+        else {
+            document.getElementById('resizable_title_div').style.width = '50vw';
+            document.getElementById('title_h2').style.opacity = '0%'
+        }
     }
 }
 
